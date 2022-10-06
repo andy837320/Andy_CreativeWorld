bac2fcbf010c97450580c96b416570654047f0a4
andy837320 <114247046+andy837320@users.noreply.github.com>
2022-10-06 17:55:24 -0400

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,357 @@
+import AcidVictim from "./img/AcidVictim.jpg";
+import Goddesh from "./img/GoddessDurga.jpg";
+import Gopal from "./img/Gopal.jpg";
+import Parents from "./img/Parents.jpg";
+import Sourav from "./img/Sourav.jpg";
+import Guruji from "./img/Guruji.jpg";
+import Krishna from "./img/Krishna.jpg";
+import Ganesha from "./img/Ganesha.jpg";
+import Akshay from "./img/Akshay.jpg";
+import Chaitali from "./img/Chaitali.JPG";
+import Diya from "./img/DiyamadewithFlour.jpg";
+import Lamp from "./img/HandmadeLamp.jpg";
+import Hrittik from "./img/Hrittik.jpg";
+import IndianPlayers from "./img/IndianPlayers.jpg";
+import Subbu from "./img/Sabnam.jpg";
+import SRK from "./img/SRK.jpg";
+import SSR from "./img/SSR.jpg";
+import Stylistgirl from "./img/Stylistgirl.jpg";
+import CreativeWork1 from "./img/CreativeWork1.jpg";
+import CreativeWork2 from "./img/CreativeWork2.jpg";
+import CreativeWork3 from "./img/CreativeWork3.jpg";
+import CreativeWork4 from "./img/CreativeWork4.jpg";
+import CreativeWork5 from "./img/CreativeWork5.jpg";
+import CreativeWork6 from "./img/CreativeWork6.jpg";
+import CreativeWork7 from "./img/CreativeWork7.jpg";
+import CreativeWork8 from "./img/CreativeWork8.jpg";
+import CreativeWork9 from "./img/CreativeWork9.jpg";
+import CreativeWork10 from "./img/CreativeWork10.jpg";
+import CreativeWork11 from "./img/CreativeWork11.jpg";
+import Kiddo1 from "./img/Kiddo1.jpg";
+import Kiddo2 from "./img/Kiddo2.jpg";
+import Kiddo3 from "./img/Kiddo3.jpg";
+import Kiddo4 from "./img/Kiddo4.jpg";
+import Kiddo5 from "./img/Kiddo5.jpg";
+import Kiddo6 from "./img/Kiddo6.jpg";
+import Flag from "./img/IndianFlagUsingCookedVeggies.jpg";
+import Apa from "./img/MadamAparajita.jpg";
+import MeWithParents from "./img/MewithParents.jpg";
+import MyCharcholWork from "./img/MyCharcholwork.jpg";
+import MyCharcholwork1 from "./img/MyCharcholwork1.jpg";
+import tshirt from "./img/Mycreativedesigntshirt.jpg";
+import tshirt1 from "./img/MycreativeTshirt.jpg";
+import tshirt2 from "./img/MyCreativeTshirt1.JPG";
+import tshirt3 from "./img/MyCreativeTshirt2.JPG";
+import tshirt4 from "./img/MyCreativeTshirt3.jpg";
+import Osman from "./img/Osmanbhai.jpg";
+import ArtWork from "./img/LovelyArtwork.jpg";
+import Priyanka from "./img/PriyankaChopra.jpg";
+import Tanushree from "./img/Tanushree.jpg";
+import HandmadeNecklace from "./img/HandmadeNecklacewithEarring.jpg";
+
+const data = {
+  cardData: [
+    {
+      id: 1,
+      img: AcidVictim,
+      title: "AcidVictim",
+      desc: "One of my fav sketching",
+    },
+    {
+      id: 2,
+      img: Goddesh,
+      title: "Goddesh Durga",
+      desc: "Ma Durga with Music",
+    },
+    {
+      id: 3,
+      img: Gopal,
+      title: "Gopal",
+      desc: "Lord(baby) Krishna",
+    },
+    {
+      id: 4,
+      img: Parents,
+      title: "Parents",
+      desc: "My Mom & Dad",
+    },
+    {
+      id: 5,
+      img: Sourav,
+      title: "Sourav",
+      desc: "My Nephew",
+    },
+    {
+      id: 6,
+      img: Ganesha,
+      title: "Ganesha",
+      desc: "Lord Ganesha",
+    },
+    {
+      id: 7,
+      img: Guruji,
+      title: "Guruji",
+      desc: "Sri Sri Ravi Shankar",
+    },
+    {
+      id: 8,
+      img: Krishna,
+      title: "Krishna",
+      desc: "Lord  Krishna",
+    },
+    {
+      id: 9,
+      img: Tanushree,
+      title: "Tanushree",
+      desc: "My Bestie from School days",
+    },
+    {
+      id: 10,
+      img: MyCharcholwork1,
+      title: "MyCharcholwork1",
+      desc: "MyCharcholwork1",
+    },
+    {
+      id: 11,
+      img: Akshay,
+      title: "Akshay Kumar",
+      desc: "Indian Bollywood star ",
+    },
+    {
+      id: 12,
+      img: Chaitali,
+      title: "Chaitali",
+      desc: "My school friend",
+    },
+    {
+      id: 13,
+      img: Diya,
+      title: "Diya",
+      desc: "Home made Diya",
+    },
+    {
+      id: 14,
+      img: Lamp,
+      title: "Lamp",
+      desc: "Handmade Lamp",
+    },
+    {
+      id: 15,
+      img: Hrittik,
+      title: "Hrittik",
+      desc: "Indian Bollywood Star",
+    },
+    {
+      id: 16,
+      img: IndianPlayers,
+      title: "IndianPlayers",
+      desc: "Indian Cricketers",
+    },
+    {
+      id: 17,
+      img: Subbu,
+      title: "Subbu",
+      desc: "My Fav Sis",
+    },
+    {
+      id: 18,
+      img: HandmadeNecklace,
+      title: "HandmadeNecklace",
+      desc: "Made this one of our biggest festival ",
+    },
+    {
+      id: 19,
+      img: SRK,
+      title: "SRK",
+      desc: "Indian Bollywood Star",
+    },
+    {
+      id: 20,
+      img: SSR,
+      title: "SSR",
+      desc: "Late Indian Bollywood Star",
+    },
+    {
+      id: 21,
+      img: Stylistgirl,
+      title: "Stylist girl",
+      desc: "Imaginative girl",
+    },
+    {
+      id: 22,
+      img: CreativeWork1,
+      title: "CreativeWork1",
+      desc: "CreativeWork1",
+    },
+    {
+      id: 23,
+      img: CreativeWork2,
+      title: "CreativeWork2",
+      desc: "CreativeWork2",
+    },
+    {
+      id: 24,
+      img: CreativeWork3,
+      title: "CreativeWork3",
+      desc: "CreativeWork3",
+    },
+    {
+      id: 25,
+      img: CreativeWork4,
+      title: "CreativeWork4",
+      desc: "CreativeWork4",
+    },
+    {
+      id: 26,
+      img: CreativeWork5,
+      title: "CreativeWork5",
+      desc: "CreativeWork5",
+    },
+    {
+      id: 27,
+      img: CreativeWork6,
+      title: "CreativeWork6",
+      desc: "CreativeWork6",
+    },
+    {
+      id: 28,
+      img: CreativeWork7,
+      title: "CreativeWork7",
+      desc: "CreativeWork7",
+    },
+    {
+      id: 29,
+      img: CreativeWork8,
+      title: "CreativeWork8",
+      desc: "CreativeWork8",
+    },
+    {
+      id: 30,
+      img: CreativeWork9,
+      title: "CreativeWork9",
+      desc: "CreativeWork9",
+    },
+    {
+      id: 31,
+      img: CreativeWork10,
+      title: "CreativeWork10",
+      desc: "CreativeWork10",
+    },
+    {
+      id: 32,
+      img: CreativeWork11,
+      title: "CreativeWork11",
+      desc: "CreativeWork11",
+    },
+    {
+      id: 33,
+      img: Kiddo1,
+      title: "Kiddo1",
+      desc: "Kiddo1",
+    },
+    {
+      id: 34,
+      img: Kiddo2,
+      title: "Kiddo2",
+      desc: "Kiddo3",
+    },
+    {
+      id: 35,
+      img: Kiddo3,
+      title: "Kiddo3",
+      desc: "Kiddo3",
+    },
+    {
+      id: 36,
+      img: Kiddo4,
+      title: "Kiddo4",
+      desc: "Kiddo4",
+    },
+    {
+      id: 37,
+      img: Kiddo5,
+      title: "Kiddo5",
+      desc: "Kiddo5",
+    },
+    {
+      id: 38,
+      img: Kiddo6,
+      title: "Kiddo6",
+      desc: "Kiddo6",
+    },
+    {
+      id: 39,
+      img: Flag,
+      title: "Indian Flag ",
+      desc: "Made with Cooked veggies",
+    },
+    {
+      id: 40,
+      img: Apa,
+      title: "Aparajita Di",
+      desc: "Madam President of BSF",
+    },
+    {
+      id: 41,
+      img: MeWithParents,
+      title: "MeWithParents",
+      desc: "This pic is really one of my cool sketch",
+    },
+    {
+      id: 42,
+      img: MyCharcholWork,
+      title: "MyCharcholWork",
+      desc: "MyCharcholWork",
+    },
+    {
+      id: 43,
+      img: tshirt,
+      title: "tshirt",
+      desc: "One of my fav tshirt painting",
+    },
+    {
+      id: 44,
+      img: tshirt1,
+      title: "tshirt1",
+      desc: "tshirt1",
+    },
+    {
+      id: 45,
+      img: tshirt2,
+      title: "tshirt2",
+      desc: "tshirt2",
+    },
+    {
+      id: 46,
+      img: tshirt3,
+      title: "tshirt3",
+      desc: "tshirt3",
+    },
+    {
+      id: 47,
+      img: tshirt4,
+      title: "tshirt4",
+      desc: "tshirt4",
+    },
+    {
+      id: 48,
+      img: Osman,
+      title: "Osman Bhai",
+      desc: "Blood Donar Hero Osman",
+    },
+    {
+      id: 49,
+      img: ArtWork,
+      title: "ArtWork",
+      desc: "These ppl are alwz close of my heart",
+    },
+    {
+      id: 50,
+      img: Priyanka,
+      title: "Priyanka",
+      desc: "Indian Bollywood Actress Priyanka Chopra",
+    },
+  ],
+};
+
+export default data;
